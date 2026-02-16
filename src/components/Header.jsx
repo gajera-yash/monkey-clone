aa89cf3ff49bdaf92284a8a7acf0c2f95e5d164c
yash gajera <gajerayash89@gmail.com>
2026-02-16 21:57:12 +0530

    erro fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useCoins } from '../context/CoinsContext';
 import { usePremium } from '../context/PremiumContext';
